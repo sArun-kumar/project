6165964bd653ab73443f97fa5a85e16a4519c010
sArun-kumar <57952933+sArun-kumar@users.noreply.github.com>
2019-11-20 17:12:12 +0530

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,1 +1,11 @@
-print("hello")
+a = int(input('enter the number of natural numbers you want '))
+sum1=0
+for i in range(1, a+1):
+    sum1=sum1+(i*i)
+
+sum2=0
+for i in range(1,a+1):
+    sum2= sum2+i
+    
+print((sum2*sum2)-sum1)
+    
